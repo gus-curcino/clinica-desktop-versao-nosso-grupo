db0806e6824892a1b44320f25d1acab05965d5ea
lorenna cristiny <lorenna.melo@aluno.imepac.edu.br>
2025-12-06 14:57:19 -0300

    config acesso lorenna

--- src/main/java/br/edu/imepac/clinica/daos/BaseDao.java
+++ src/main/java/br/edu/imepac/clinica/daos/BaseDao.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-// lorennatest
+// lorennateste
 package br.edu.imepac.clinica.daos;
 
 import java.sql.Connection;
